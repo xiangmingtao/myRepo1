67562c9f8c3050f461e0df1ebdf8645347e1f672
xiangmingtao <xiangmingtao@foxmail.com>
2017-11-09 13:07:56 +0800

    更新类注释

--- WebCollector/src/com/css/webspider/CommonHttpUtils.java
+++ WebCollector/src/com/css/webspider/CommonHttpUtils.java
@@ -5,10 +5,10 @@ package com.css.webspider;
  *
  * @project 尽职调查项目
  * @package com.css.webspider
- * @file CommonHttpUtils.java 创建时间:2017-11-8下午07:47:00
+ * @file CommonHttpUtils.java 创建时间:2017-11-9下午01:07:28
  * @title 标题（要求能简洁地表达出类的功能和职责）
  * @description 描述（简要描述类的职责、实现方式、使用注意事项等）
- * @copyright Copyright (c) 2017 中国软件与技术服务股份有限公司
+ * @copyright Copyright (c) 2017 中伯伦（北京）信息技术有限公司
  * @company 中伯伦（北京）信息技术有限公司
  * @module 模块: 模块名称
  * @author   项明涛
